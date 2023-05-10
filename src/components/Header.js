5256ebba9941fc0390ade5b6c6297133cc0d9528
Rishabh-Singh-Codes <rishabhsinghcodes@gmail.com>
2023-05-10 21:05:51 +0530

    Added basic setup

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import { StyledHeader } from "./styles/Header.styled"
+
+const Header = () => {
+  return (
+    <StyledHeader>
+        <h1>Huddle</h1>
+    </StyledHeader>
+  )
+}
+
+export default Header
